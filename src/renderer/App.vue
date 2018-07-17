24ce952a35460118cbbe33755f2a5339f1f3a639
sunkai07 <sunkai07@baidu.com>
2018-07-17 22:29:30 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'z2devtool'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
